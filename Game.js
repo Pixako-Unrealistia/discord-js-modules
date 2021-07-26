1ce911e636e1153f4a1db03e3f28f698bacc4919
Pixako <62527607+Pixako-Unrealistia@users.noreply.github.com>
2021-07-26 13:42:55 +0700

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,27 @@
+var { MessageEmbed } = require("discord.js");
+
+
+module.exports = {
+  name: "together",
+  description: "filler description",
+  run: async (client, message, args) => {
+    console.log("I am running too");
+    message.channel.send("Available types: youtube,poker,chess,betrayal,fishing. Please type whatever you wish to choose in a precise manner.")
+      try{
+
+      let msgs = await message.channel.awaitMessages(
+        (u2) => u2.author.id === message.author.id,
+        { time: 16900, max: 1, errors: ["time"] }
+      );
+
+      client.discordTogether.createTogetherCode(message.member.voice.channelID, msgs.first().content).then(async invite => {
+      return message.channel.send(`${invite.code}`);
+      });
+
+
+    } catch (e) {
+      return message.channel.send(`Time out.`);
+    }
+
+}
+}
